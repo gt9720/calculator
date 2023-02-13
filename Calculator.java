d858f0e3f36b60bc3c9b513b8098e4f56ca5086e
Greg9720 <greg.thompson9720@gmail.com>
2023-02-12 16:41:36 -0800

    Our colleage updates adding Addition method for double numbs

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,11 @@ public class Calculator
 	public static void main(String[] args) 
 	{
 		public int addition(int a, int b)
-			{return a + b;}
+			return a + b;
 	}
+
+      public double addition(double a, double b)
+	{
+		return a + b;
+	} 
 }
