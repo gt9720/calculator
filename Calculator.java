5fe58eb402bf6cc239ee8865b3d39b8992129907
Greg9720 <greg.thompson9720@gmail.com>
2023-02-14 20:34:49 -0800

    Reconciled changes

--- Calculator.java
+++ Calculator.java
@@ -12,4 +12,18 @@ public class Calculator
 				return a + b;
 			}
 	} 
+
+      public double addition(double a, double b)
+	{
+		return a + b;
+	}
+
+	public int subtraction(int a, int b)
+	{
+		return a - b;
+	}
+
+	public double subtraction(double a, double b)
+	{
+		return a - b;
 }
